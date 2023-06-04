562688f90fb6cf3f711f489ba740f1c76092cb99
Muhammad Muddasar Sabir <mudasarsabir@gmail.com>
2023-06-04 22:45:37 +0500

    ad

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 x = st.slider('x')
 st.write(x, 'squared is', x * x)
+
+# Reuse this data across runs!
+read_and_cache_csv = st.cache(pd.read_csv)
+
+BUCKET = "https://streamlit-self-driving.s3-us-west-2.amazonaws.com/"
+data = read_and_cache_csv(BUCKET + "labels.csv.gz", nrows=1000)
+desired_label = st.selectbox('Filter to:', ['car', 'truck'])
+st.write(data[data.label == desired_label])
